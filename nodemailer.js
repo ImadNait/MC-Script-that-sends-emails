f9ad12dadfa4c428dd2ce4c0ef95f29f411d6fed
Imad <153989803+ImadNait@users.noreply.github.com>
2024-06-03 23:09:18 +0100

    Update nodemailer.js

--- nodemailer.js
+++ nodemailer.js
@@ -1,4 +1,5 @@
 const nodemailer = require("nodemailer");
+require("dotenv").config();
 const path = require('path');
 const transporter = nodemailer.createTransport({
     host:'stmp.gmail.com',
@@ -6,13 +7,13 @@ const transporter = nodemailer.createTransport({
     service:'gmail',
     secure:false,
     auth:{
-        user:"philnait2013@gmail.com",
-        pass:"gbnz kvps cmtv uzmk",
+        user:process.env.user_smtp,
+        pass:process.env.pass_smtp,
     }
     })
 
     const mailOptions ={
-    from:"philnait2013@gmail.com",
+    from:pass_smtp,
     to:"naitmihoubimzd@gmail.com",
     subject:`Sending Email using NodeJS`,
     text:'Heyy wassup',
